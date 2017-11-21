8e9daa3f5d09756f25733356bdc9fd4dcc0b2d9c
Nick-Rimmer2016 <nick.rimmer@ndr-itsolutions.co.uk>
2017-11-21 22:11:57 +0000

    Update Stuff.ps1

--- Stuff.ps1
+++ Stuff.ps1
@@ -11,3 +11,27 @@ Start-Transcript -Path $logFile -Append -
 "I received $($a.Count) services."
 Write-Host "Watch out: direct output will not be logged!"
 
+# File info
+$logFile = "$PSScriptRoot\mylog.txt"
+ 
+$exists = Test-Path -Path $logFile
+if ($exists)
+{
+  $data = Get-Item -Path $logFile
+  if ($data.Length -gt 100KB)
+  {
+    Remove-Item -Path $logFile
+  }
+ 
+} 
+
+#Passwords
+$Path = "$home\Desktop\multipass.xml"
+ 
+[PSCustomObject]@{
+    User1 = Get-Credential -Message User1
+    User2 = Get-Credential -Message User2
+    User3 = Get-Credential -Message User3
+} | Export-Clixml -Path $Path  
+
+$multipass = Import-Clixml -Path $Path 
