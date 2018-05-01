d2442a9a9927118768edc7d04f03211673ecae89
Nick-Rimmer2016 <nick.rimmer@ndr-itsolutions.co.uk>
2018-05-01 12:12:08 +0100

    VDA Build

---
+++ BuildVDA.ps1
@@ -0,0 +1,71 @@
+﻿pushd c:\files
+configuration BuildVDA
+{
+
+param (
+        [string[]]$Computername
+      )
+        
+Import-DscResource –ModuleName PSDesiredStateConfiguration
+Import-DscResource -ModuleName xPSDesiredStateConfiguration
+Import-DscResource -ModuleName xWindowsupdate
+
+
+     node $Computername
+    {
+        
+        Package Silverlight
+        {
+           Ensure = "Present"
+           Name = "Microsoft Silverlight"
+           Path = "C:\Files\Silverlight_x64.exe"
+           ProductID = "89F4137D-6C26-4A84-BDB8-2E5A4BB71E00"
+           Arguments = "/q /noupdate /noreboot"
+        }
+        Package AcrobatDC
+        {
+           Ensure = "Present"
+           Name = "Adobe Acrobat Reader DC"
+           Path = "C:\Files\AcroRdrDC1801120035_en_US.exe"
+           ProductID = "AC76BA86-7AD7-1033-7B44-AC0F074E4100"
+           Arguments = "/sPB /rs /msi"
+        }
+        xHotFix Powershell
+        {
+            Path="c:\files\Win8.1AndW2K12R2-KB3191564-x64.msu"
+            Id="KB3191564"
+            Ensure="Present"
+        }
+        WindowsFeature RDS
+        {
+            Ensure = "Present"
+            Name = "RDS-RD-Server"
+        }
+
+        WindowsFeature ASPDotNet
+        {
+            Ensure = "Present"
+            Name = "NET-Framework-45-ASPNET"
+        }
+
+        File Scripts
+        {
+            Ensure = "Present"
+            Type = "Directory"
+            SourcePath = "c:\files\scripts"
+            DestinationPath = "e:\scripts"
+            Recurse = $true
+        }
+
+        File Logs
+        {
+            Ensure = "Present"
+            Type = "Directory"
+            DestinationPath = "e:\log"
+        }
+    
+    }
+}
+
+BuildVDA -Computername PSDEVOPS-APP4 
+Start-DscConfiguration -Wait -Path C:\files\BuildVDA  -Verbose -Force
---
+++ PrepVM.ps1
@@ -0,0 +1,74 @@
+﻿# Pre-Req's for Building VM
+
+# Set IE Enh to off
+function Disable-ieESC {
+    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+    Stop-Process -Name Explorer
+    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+}
+Disable-ieESC
+
+# Disable Server Manager on logon
+function DisableSrvManager {
+    New-ItemProperty -Path HKCU:\Software\Microsoft\ServerManager -Name DoNotOpenServerManagerAtLogon -PropertyType DWORD -Value "0x1" –Force}
+DisableSrvManager
+
+# Resize OS partition, Create New partition for applications
+# Check if it's been partitioned
+function ctxResize {
+$part = Get-Partition | where {$_.DriveLetter -eq 'C'}
+Resize-Partition -DiskNumber $part.DiskNumber -PartitionNumber $part.PartitionNumber -Size (80gb)
+New-Partition -DiskNumber $part.DiskNumber -UseMaximumSize -DriveLetter E
+$currentconfirm = $confirmpreference
+$confirmpreference = 'none'
+Format-Volume -Driveletter E -NewFileSystemLabel 'Data'
+    $confirmpreference = $currentconfirm }
+
+
+# Configure LCM
+[DSCLocalConfigurationManager()]
+configuration LCMConfig
+{
+    Node localhost
+    {
+    Settings
+
+        {
+            RefreshMode        = 'Push'
+            RebootNodeIfNeeded = $True
+        }
+    }
+}
+LCMConfig
+Set-DscLocalConfigurationManager -Path C:\Files\LCMConfig -Force
+
+
+#Citrix Install
+$arg0 = " /quiet /masterimage /disableexperiencemetrics /components VDA"
+$arg0 += " /masterimage /virtualmachine /optimize"
+#$arg0 += " /noreboot"
+Start-Process -FilePath C:\files\VDAServerSetup_7.17.exe `
+-ArgumentList $arg0
+
+
+function Write-Log {
+     [CmdletBinding()]
+     param(
+         [Parameter()]
+         [ValidateNotNullOrEmpty()]
+         [string]$Message,
+ 
+         [Parameter()]
+         [ValidateNotNullOrEmpty()]
+         [ValidateSet('Information','Warning','Error')]
+         [string]$Severity = 'Information'
+     )
+ 
+     [pscustomobject]@{
+         Time = (Get-Date -f g)
+         Message = $Message
+         Severity = $Severity } | Export-Csv -Path "c:\files\LogFile.csv" -Append -NoTypeInformation
+ }
