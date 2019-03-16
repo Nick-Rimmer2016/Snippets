6019f124dc6728bae225946f9bfa480e92c81ec9
Nick-Rimmer2016 <nick.rimmer.2009@gmail.com>
2019-03-16 13:06:37 +0000

    Update Remove-TempJEAAdminAccounts.ps1

--- Remove-TempJEAAdminAccounts.ps1
+++ Remove-TempJEAAdminAccounts.ps1
@@ -5,10 +5,8 @@
    This is a bug that exists in Windows Server 2012 R2, where after a JEA session with the virtual account setting 
    switch on, the session does not exit cleanly and leave the virtual account in logon as a service.
    https://github.com/PowerShell/PowerShell/issues/5296 (This has been fixed in 2016 & 2019)
-   The Userrights module from the following web site is required:
-   https://gallery.technet.microsoft.com/scriptcenter/Grant-Revoke-Query-user-26e259b0/view/Discussions#content
 .EXAMPLE
-   Remove-TempJEAAdminAccounts -backup c:\  (Only specify drive and folder here), the file will be assigned the name 
+   Remove-TempJEAAdminAccounts -backup c:\backups  (Only specify drive and folder here), the file will be assigned the name 
    SeServiceLogonRight-backup.txt
 
 #>
@@ -19,25 +17,27 @@ function Remove-TempJEAAdminAccounts
     [OutputType([int])]
     Param
     (
-        # Param1 help description
-        [Parameter(Mandatory=$true,
+        # Backup Param description
+        [Parameter(Mandatory=$false,
                    ValueFromPipelineByPropertyName=$true,
                    Position=0)]
-        $backup
+        [string]$backup = "c:"
     )
-
+    if (Get-Module -ListAvailable -Name UserRights) {Write-Verbose "Module exists"} 
+                else {Write-Verbose "Module does not exist"; exit 0}
+             
     Write-Verbose "Gather Account List"
     $names = (Get-AccountsWithUserRight -Right SeServiceLogonRight).Account
-    Write-Verbose "Output Accounts to a text file for backup $backup+SeServiceLogonRight-backup.txt"
+    Write-Verbose "Output Accounts to a text file for backup $backup\SeServiceLogonRight-backup.txt"
     $names | Out-file $backup\SeServiceLogonRight-backup.txt
 
     $names | foreach {
 
            if ($_ -Match 'WinRM Virtual Users*')
-
-               {
-                 Revoke-UserRight -Right SeServiceLogonRight -Account $_ 
-               }
-           else {Write-Verbose "No JEA Temp Admin Accounts present"}
+            {
+                Write-Verbose "Deleting $_"
+                Revoke-UserRight -Right SeServiceLogonRight -Account $_
+            }
+           else {Write-Verbose "Keeping Account $_"}
         }
 }
