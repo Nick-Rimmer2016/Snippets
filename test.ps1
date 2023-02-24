c07cd743130858422da60c92905dac6b70fda03a
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-02-24 16:06:21 +0000

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -2,16 +2,24 @@
 $proxyServer = "proxy.example.com:8080"
 
 # Set the web sites to add to Trusted sites
-$trustedSites = @("http://example.com", "http://www.example.com")
+$trustedSites = @("https://example.com", "https://www.example.com")
 
 # Set the registry key paths for Internet Explorer settings
 $registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
 
-# Enable the proxy server
-Set-ItemProperty -Path $registryPath -Name ProxyEnable -Value 1
-Set-ItemProperty -Path $registryPath -Name ProxyServer -Value $proxyServer
+# Test connection to the proxy server
+$testConnection = Test-NetConnection -ComputerName $proxyServer -Port 8080
+if ($testConnection.TcpTestSucceeded -eq $false) {
+    Write-Host "Unable to connect to the proxy server on port 8080. Proxy will not be enabled." -ForegroundColor Red
+} else {
+    # Enable the proxy server
+    Set-ItemProperty -Path $registryPath -Name ProxyEnable -Value 1
+    Set-ItemProperty -Path $registryPath -Name ProxyServer -Value $proxyServer
 
-# Add the web sites to Trusted sites
-$trustedSitesValue = $trustedSites -join "`n"
-Set-ItemProperty -Path $registryPath -Name TrustedSites -Value $trustedSitesValue
-Set-ItemProperty -Path $registryPath -Name "ZoneMapKey" -Value 2 -Type DWORD
+    # Add the web sites to Trusted sites
+    $trustedSitesValue = $trustedSites -join "`n"
+    Set-ItemProperty -Path $registryPath -Name TrustedSites -Value $trustedSitesValue
+    Set-ItemProperty -Path $registryPath -Name "ZoneMapKey" -Value 2 -Type DWORD
+
+    Write-Host "Proxy server enabled and web sites added to Trusted sites." -ForegroundColor Green
+}
