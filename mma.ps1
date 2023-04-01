0be7a61cefdd729b5a40083ad6a87b2fe28bbed7
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-04-01 17:31:01 +0100

    Update mma.ps1

--- mma.ps1
+++ mma.ps1
@@ -1,38 +1,37 @@
-# Create a listener on port 1522
-$listener1 = New-Object System.Net.Sockets.TcpListener([IPAddress]::Any, 1522)
-$listener1.Start()
+# Import the powershell-yaml module
+Import-Module powershell-yaml
 
-# Create a listener on port 1523
-$listener2 = New-Object System.Net.Sockets.TcpListener([IPAddress]::Any, 1523)
-$listener2.Start()
+# Load the YAML content from the file
+$yamlContent = Get-Content -Path "servers_ports.yaml" -Raw
+$serversData = ConvertFrom-Yaml -Yaml $yamlContent
 
-# Create a listener on port 22
-$listener3 = New-Object System.Net.Sockets.TcpListener([IPAddress]::Any, 22)
-$listener3.Start()
-
-# Keep the script running to continue listening
-while ($true) {
-    # Check if there is a client waiting to connect to listener1
-    if ($listener1.Pending()) {
-        # Accept the client connection and handle it
-        $client1 = $listener1.AcceptTcpClient()
-        # Handle the client connection here
+# Function to test remote port connectivity
+function Test-RemotePort ($host, $port, $protocol) {
+    $result = Test-NetConnection -ComputerName $host -Port $port
+    if ($result.TcpTestSucceeded) {
+        Write-Host "$protocol ($port) is open on $host" -ForegroundColor Green
+    } else {
+        Write-Host "$protocol ($port) is closed on $host" -ForegroundColor Red
     }
+}
 
-    # Check if there is a client waiting to connect to listener2
-    if ($listener2.Pending()) {
-        # Accept the client connection and handle it
-        $client2 = $listener2.AcceptTcpClient()
-        # Handle the client connection here
+# Get the local hostname
+$localHostName = [System.Net.Dns]::GetHostName()
+
+# Filter the servers data based on the local hostname
+$serversData.servers = $serversData.servers | Where-Object { $_.server -eq $localHostName }
+
+# Check if the local hostname is in the YAML file
+if ($serversData.servers -eq $null) {
+    Write-Host "The local hostname '$localHostName' is not found in the YAML file. No ports will be tested." -ForegroundColor Yellow
+    exit
+}
+
+# Iterate through the YAML data and test each server and port
+foreach ($serverData in $serversData.servers) {
+    $remoteHost = $serverData.server
+    foreach ($protocol in $serverData.ports.Keys) {
+        $port = $serverData.ports[$protocol]
+        Test-RemotePort -host $remoteHost -port $port -protocol $protocol
     }
-
-    # Check if there is a client waiting to connect to listener3
-    if ($listener3.Pending()) {
-        # Accept the client connection and handle it
-        $client3 = $listener3.AcceptTcpClient()
-        # Handle the client connection here
-    }
-
-    # Add a delay to avoid using too much CPU
-    Start-Sleep -Milliseconds 100
 }
