571927d84f6bcf92c97f00f813d2a58cb86e5050
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-04-05 10:46:29 +0100

    Update mma.ps1

--- mma.ps1
+++ mma.ps1
@@ -1,37 +1,19 @@
-# Import the powershell-yaml module
-Import-Module powershell-yaml
+# Import the CSV file
+$servers = Import-Csv -Path "C:\Path\To\File.csv"
 
-# Load the YAML content from the file
-$yamlContent = Get-Content -Path "servers_ports.yaml" -Raw
-$serversData = ConvertFrom-Yaml -Yaml $yamlContent
+# Load the YAML file
+$config = Get-Content -Path "C:\Path\To\Config.yaml" -Raw | ConvertFrom-Yaml
 
-# Function to test remote port connectivity
-function Test-RemotePort ($host, $port, $protocol) {
-    $result = Test-NetConnection -ComputerName $host -Port $port
-    if ($result.TcpTestSucceeded) {
-        Write-Host "$protocol ($port) is open on $host" -ForegroundColor Green
-    } else {
-        Write-Host "$protocol ($port) is closed on $host" -ForegroundColor Red
-    }
+# Define the rule engine
+$ruleEngine = @{
+    "JK*" = $config.JKValue
+    "Server2" = $config.Server2Value
+    # Add more rules here as needed
 }
 
-# Get the local hostname
-$localHostName = [System.Net.Dns]::GetHostName()
+# Apply the rule engine to extract lines from the CSV file
+$filteredServers = $servers | Where-Object { $ruleEngine.ContainsKey($_.ServerName) }
 
-# Filter the servers data based on the local hostname
-$serversData.servers = $serversData.servers | Where-Object { $_.server -eq $localHostName }
+# Output the filtered servers
+$filteredServers
 
-# Check if the local hostname is in the YAML file
-if ($serversData.servers -eq $null) {
-    Write-Host "The local hostname '$localHostName' is not found in the YAML file. No ports will be tested." -ForegroundColor Yellow
-    exit
-}
-
-# Iterate through the YAML data and test each server and port
-foreach ($serverData in $serversData.servers) {
-    $remoteHost = $serverData.server
-    foreach ($protocol in $serverData.ports.Keys) {
-        $port = $serverData.ports[$protocol]
-        Test-RemotePort -host $remoteHost -port $port -protocol $protocol
-    }
-}
