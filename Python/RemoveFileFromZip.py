4c698ce0c1de51a86125df1df7a5f3090949fd31
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-08-21 13:33:57 +0100

    Update RemoveFileFromZip.py

--- Python/RemoveFileFromZip.py
+++ Python/RemoveFileFromZip.py
@@ -11,13 +11,13 @@ def delete_from_zip(zip_filename, files_to_delete, password=None):
             zip_read.pwd = password.encode('utf-8')
         
         # Open the temporary ZIP file in write mode
-        with pyzipper.AESZipFile(temp_zip, 'w') as zip_write:
+        with pyzipper.AESZipFile(temp_zip, 'w', compression=pyzipper.ZIP_DEFLATED) as zip_write:
             # Go through each file in the original ZIP
             for item in zip_read.infolist():
                 if item.filename not in files_to_delete:
                     with zip_read.open(item.filename) as file:
                         data = file.read()
-                        zip_write.writestr(item.filename, data)
+                        zip_write.writestr(item, data)  # Pass the ZipInfo object directly
                     if password:
                         zip_write.pwd = password.encode('utf-8')
     
