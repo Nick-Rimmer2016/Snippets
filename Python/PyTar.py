0a4c5af20e0d14e8f81542170c49ef6a98b8e369
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-09-06 16:54:28 +0100

    Updated to check existing files

--- Python/PyTar.py
+++ Python/PyTar.py
@@ -9,21 +9,26 @@ def tar_files(src_folder, dest_folder, archive_name, max_size=1073741824):
     tar_path = os.path.join(dest_folder, f"{archive_name}_{tar_count}.tar")
     tar = tarfile.open(tar_path, "w")
     
+    already_added = set()
+    
     for foldername, subfolders, filenames in os.walk(src_folder):
         for subfolder in subfolders:
             path = os.path.join(foldername, subfolder)
-            tar.add(path, arcname=path[len(src_folder)+1:])
-
+            if path not in already_added:
+                tar.add(path, arcname=path[len(src_folder)+1:])
+                already_added.add(path)
+            
             if get_size(tar_path) >= max_size:
                 tar.close()
                 tar_count += 1
                 tar_path = os.path.join(dest_folder, f"{archive_name}_{tar_count}.tar")
                 tar = tarfile.open(tar_path, "w")
-                break
 
         for filename in filenames:
             path = os.path.join(foldername, filename)
-            tar.add(path, arcname=path[len(src_folder)+1:])
+            if path not in already_added:
+                tar.add(path, arcname=path[len(src_folder)+1:])
+                already_added.add(path)
             
             if get_size(tar_path) >= max_size:
                 tar.close()
@@ -33,11 +38,12 @@ def tar_files(src_folder, dest_folder, archive_name, max_size=1073741824):
 
     tar.close()
 
+# Example usage
 src_folder = "d:\\source"  # Replace with your source folder
 dest_folder = "d:\\tar"  # Replace with your destination folder
 archive_name = "my_archive"  # Replace with your desired archive name
 
-# Make sure destination directory exists
+# Make sure the destination directory exists
 if not os.path.exists(dest_folder):
     os.makedirs(dest_folder)
 
