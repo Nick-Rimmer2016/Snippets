8ff194e3a216343ddb2b46bb55b6a306a9154e25
Nick Rimmer <nick.rimmer.2009@gmail.com>
2023-09-06 16:33:47 +0100

    Update PyTar.py

--- Python/PyTar.py
+++ Python/PyTar.py
@@ -4,31 +4,40 @@ import tarfile
 def get_size(file_path):
     return os.path.getsize(file_path)
 
-def tar_files(src_folder, dest_folder, max_size=1073741824):  # max_size in bytes (1GB)
+def tar_files(src_folder, dest_folder, archive_name, max_size=1073741824):
     tar_count = 1
-    tar = tarfile.open(f"{dest_folder}/archive_{tar_count}.tar", "w")
+    tar_path = os.path.join(dest_folder, f"{archive_name}_{tar_count}.tar")
+    tar = tarfile.open(tar_path, "w")
 
     for foldername, subfolders, filenames in os.walk(src_folder):
         for subfolder in subfolders:
             path = os.path.join(foldername, subfolder)
-            tar.add(path, arcname=path[len(src_folder):])
+            tar.add(path, arcname=path[len(src_folder)+1:])
 
-            if get_size(f"{dest_folder}/archive_{tar_count}.tar") > max_size:
+            if get_size(tar_path) >= max_size:
                 tar.close()
                 tar_count += 1
-                tar = tarfile.open(f"{dest_folder}/archive_{tar_count}.tar", "w")
+                tar_path = os.path.join(dest_folder, f"{archive_name}_{tar_count}.tar")
+                tar = tarfile.open(tar_path, "w")
 
         for filename in filenames:
             path = os.path.join(foldername, filename)
-            tar.add(path, arcname=path[len(src_folder):])
+            tar.add(path, arcname=path[len(src_folder)+1:])
 
-            if get_size(f"{dest_folder}/archive_{tar_count}.tar") > max_size:
+            if get_size(tar_path) >= max_size:
                 tar.close()
                 tar_count += 1
-                tar = tarfile.open(f"{dest_folder}/archive_{tar_count}.tar", "w")
+                tar_path = os.path.join(dest_folder, f"{archive_name}_{tar_count}.tar")
+                tar = tarfile.open(tar_path, "w")
 
     tar.close()
 
-src_folder = "path/to/source/folder"
-dest_folder = "path/to/destination/folder"
-tar_files(src_folder, dest_folder)
+src_folder = "d:\\source_folder"  # Replace with your source folder
+dest_folder = "d:\\tar"  # Replace with your destination folder
+archive_name = "my_archive"  # Replace with your desired archive name
+
+# Make sure destination directory exists
+if not os.path.exists(dest_folder):
+    os.makedirs(dest_folder)
+
+tar_files(src_folder, dest_folder, archive_name)
