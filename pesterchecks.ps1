edd159e240c9b7d2ac8766408a6fae0335a01c6e
Nick Rimmer <nick.rimmer.2009@gmail.com>
2024-08-09 14:47:28 +0100

    Update pesterchecks.ps1

--- pesterchecks.ps1
+++ pesterchecks.ps1
@@ -1,11 +1,42 @@
-It 'RDP should be enabled' {
-        # Path to the RDP registry key
-        $regPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
-        $valueName = 'fDenyTSConnections'
-        
-        # Get the value of the RDP setting
-        $rdpSetting = Get-ItemProperty -Path $regPath -Name $valueName
-        
-        # Check if the setting exists and is set to 0 (0 means RDP is enabled)
-        $rdpSetting.$valueName | Should Be 0
+# Define the Pester test block
+Describe 'Windows Server UK Configuration' {
+
+    # Test the Time Zone
+    It 'Should have the correct time zone set to UK time zone' {
+        $timeZone = Get-TimeZone
+        $expectedTimeZones = @('GMT Standard Time', 'GMT Daylight Time')
+        $expectedTimeZones -contains $timeZone.Id | Should -Be $true
     }
+
+    # Test the System Locale
+    It 'Should have the system locale set to en-GB' {
+        $systemLocale = Get-WinSystemLocale
+        $systemLocale.Name | Should -Be 'en-GB'
+    }
+
+    # Test the Input Language
+    It 'Should have the input language set to en-GB' {
+        $inputLanguage = Get-WinUserLanguageList
+        $inputLanguage[0].LanguageTag | Should -Be 'en-GB'
+    }
+
+    # Test the Currency Format
+    It 'Should have the currency format set to £' {
+        $currencySymbol = (Get-Culture).NumberFormat.CurrencySymbol
+        $currencySymbol | Should -Be '£'
+    }
+
+    # Test the Date Format
+    It 'Should have the short date pattern set to dd/MM/yyyy' {
+        $dateFormat = (Get-Culture).DateTimeFormat.ShortDatePattern
+        $dateFormat | Should -Be 'dd/MM/yyyy'
+    }
+
+    # Test the Time Format
+    It 'Should have the time format set to HH:mm:ss' {
+        $timeFormat = (Get-Culture).DateTimeFormat.LongTimePattern
+        $timeFormat | Should -Be 'HH:mm:ss'
+    }
+
+}
+
