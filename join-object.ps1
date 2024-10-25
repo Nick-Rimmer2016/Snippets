16c03f4a3dd8e5de26eac274903b152eb6de55d6
Nick Rimmer <nick.rimmer.2009@gmail.com>
2024-10-25 14:43:29 +0100

    Update join-object.ps1

--- join-object.ps1
+++ join-object.ps1
@@ -13,14 +13,24 @@ function Join-Object {
         $matched = $false
         foreach ($rightItem in $Right) {
             if ($leftItem.$LeftJoinField -eq $rightItem.$RightJoinField) {
-                $joinedArray += [PSCustomObject]@{
-                    # Fields from both CSVs
-                    Accounts     = $leftItem.$LeftJoinField
-                    Account_Info = $rightItem
+                # Combine fields from both CSVs
+                $combinedObject = [PSCustomObject]@{
+                    # Include all fields from the left item
+                    PSObject.Properties.AddRange($leftItem.PSObject.Properties)
                 }
+
+                # Include all fields from the right item, avoiding duplicates
+                foreach ($property in $rightItem.PSObject.Properties) {
+                    if (-not $combinedObject.PSObject.Properties.Match($property.Name)) {
+                        $combinedObject | Add-Member -MemberType NoteProperty -Name $property.Name -Value $property.Value
+                    }
+                }
+
+                $joinedArray += $combinedObject
                 $matched = $true
             }
         }
+
         if ($JoinType -eq "Left" -and -not $matched) {
             $joinedArray += $leftItem
         }
